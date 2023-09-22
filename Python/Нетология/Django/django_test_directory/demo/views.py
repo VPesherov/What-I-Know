be35fd6c3a2e1cf8d86a97fc58ebb6d67accf3cf
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-09-22 15:42:16 +0700

    Continued Django learning netology

--- Python/Нетология/Django/django_test_directory/demo/views.py
+++ Python/Нетология/Django/django_test_directory/demo/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+
 
 # Create your views here.
 
-
+def hello_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Hello World!')
--- Python/Нетология/Django/django_test_directory/django_test/urls.py
+++ Python/Нетология/Django/django_test_directory/django_test/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from demo.views import hello_view # импортируем
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view) # добавляем сюда
 ]
