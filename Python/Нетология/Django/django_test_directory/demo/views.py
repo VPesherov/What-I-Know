c96ef2610d7fd73fd75e69b3003227d00ad24b55
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-09-25 13:30:03 +0700

    django templates netology

--- Python/Нетология/Django/django_test_directory/demo/views.py
+++ Python/Нетология/Django/django_test_directory/demo/views.py
@@ -5,9 +5,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_view(request: HttpRequest) -> HttpResponse:
-    name = request.GET.get("name")
-    age = int(request.GET.get("age", 20))  # 20 - это дефолтное значение - если ничего не найдено
-    return HttpResponse(f'Hello {name}, {age}')
+    context = {
+        'test': 5,
+        'array': [1, 5, 9]
+    }
+    return render(request, 'demo.html', context=context) # путь к шаблонку указывается относительно папки templates
+    # name = request.GET.get("name")
+    # age = int(request.GET.get("age", 20))  # 20 - это дефолтное значение - если ничего не найдено
+    # return HttpResponse(f'Hello {name}, {age}')
 
 
 def sum1(request, a, b):
